6f90a654e509343c251e7c6b57eb740314720cb8
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-10-30 01:40:06 +0300

    build: Настройка окружения для тестов

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/todo_test/",
+  base: '/todo_test/',
   plugins: [react()],
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+  },
+})
