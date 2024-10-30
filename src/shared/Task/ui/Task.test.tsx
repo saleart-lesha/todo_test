dcc313e06029ece78453466c4a139167c7da50e3
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-10-31 00:18:38 +0300

    test: Добавил тест для Task
    - добавил тест на изменение статуса задачи

---
+++ src/shared/Task/ui/Task.test.tsx
@@ -0,0 +1,40 @@
+import { Card, MantineProvider } from '@mantine/core'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { http } from 'msw'
+import { Provider } from 'react-redux'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { store } from '../../../app/store/store'
+import { server } from '../../../app/test/setupTests'
+import { TaskApi } from '../api/TaskApi'
+import Task from './Task'
+
+describe('TaskAction', () => {
+  beforeEach(() => {
+    server.resetHandlers()
+    store.dispatch(TaskApi.util.resetApiState())
+  })
+  it('Изменение статуса выполнения задачи', async () => {
+    let requestBody = null
+    server.use(
+      http.put('/tasks/1', async ({ request }) => {
+        requestBody = await request.json()
+      }),
+    )
+
+    render(
+      <Provider store={store}>
+        <MantineProvider>
+          <Card>
+            <Task task={{ id: '1', taskName: 'task', completed: false }} />
+          </Card>
+        </MantineProvider>
+      </Provider>,
+    )
+
+    fireEvent.click(screen.getByRole('checkbox'))
+
+    await waitFor(() => {
+      expect(requestBody).toEqual({ taskName: 'task', completed: true })
+    })
+  })
+})
