8a870f5d51a97f767d568731151408e64a7843f8
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-11-01 01:03:06 +0300

    refacor: Убрал console.log

--- src/app/services/TodoService.ts
+++ src/app/services/TodoService.ts
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-console.log('API URL:', import.meta.env.VITE_API_URL)
 export const TodoService = createApi({
   reducerPath: 'TodoService',
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
